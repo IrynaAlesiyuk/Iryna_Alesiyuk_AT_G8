0cfd5ddcf71940ec0e6eaacd6c4a16b59e1d692c
Iryna Alesiyuk <oira2008@yandex.ru>
2023-10-25 10:25:58 +0300

    task 0.1

---
+++ src/homework/day8/someClasses/Person.java
@@ -0,0 +1,43 @@
+package homework.day8.someClasses;
+
+import java.util.Objects;
+
+public class Person {
+
+    private int age;
+    private String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
