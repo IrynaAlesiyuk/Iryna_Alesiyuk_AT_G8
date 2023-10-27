0ba79ce00203975d705f26322fe1ed48c8873154
Iryna Alesiyuk <oira2008@yandex.ru>
2023-10-27 20:49:02 +0300

    rename task8 to task9

--- src/homework/day8/collectionTask/TaskNine.java
+++ src/homework/day8/collectionTask/TaskNine.java
@@ -1,10 +1,9 @@
 package homework.day8.collectionTask;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class TaskEight {
+public class TaskNine {
 
     public static void main(String[] args) {
         List<Double> doubles = Arrays.asList(33.42, 34.3, 0.79, 2.3426, 6.8, 13.24, 5.5, 769.9);
