8574f3aac5f00ffcd7aaa7807def555c8f4dacdb
Iryna Alesiyuk <oira2008@yandex.ru>
2023-10-30 14:49:35 +0300

    task4

--- src/homework/day9/AutoRunner.java
+++ src/homework/day9/AutoRunner.java
@@ -1,10 +1,35 @@
 package homework.day9;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class AutoRunner {
-    //Создать поток данных стрингов из 7 авто (Мерс, Ауди, Жигуль, Рено, Жигуль, Жигуль, Ауди) (через Stream.of())
-    //Оставить в потоке только уникальные названия авто
-    //Отфильтровать только те названия, которые содержат букву "и"
-    //Пропустить 1 название
-    //Все названия привести в верхний регистр
-    //Отпечатать все названия в консоль с новой строки
+
+    public static void main(String[] args) {
+        //Создать поток данных стрингов из 7 авто (Мерс, Ауди, Жигуль, Рено, Жигуль, Жигуль, Ауди)
+        // (через Stream.of())
+        Stream<String> carStream = Stream.of("Мерс", "Ауди", "Жигуль", "Рено", "Жигуль", "Жигуль", "Ауди");
+
+        //Оставить в потоке только уникальные названия авто
+        List<String> distinctCarList = carStream.distinct().toList();
+        distinctCarList.forEach(System.out::println);
+        System.out.println("---------");
+
+        //Отфильтровать только те названия, которые содержат букву "и"
+        List<String> iList = distinctCarList.stream().filter(el -> el.contains("и")).toList();
+        iList.forEach(System.out::println);
+        System.out.println("---------");
+
+        //Пропустить 1 название
+        List<String> skipFirstList = iList.stream().skip(1).toList();
+        skipFirstList.forEach(System.out::println);
+        System.out.println("---------");
+
+        //Все названия привести в верхний регистр
+        List<String> upperCaseList = skipFirstList.stream().map(String::toUpperCase).toList();
+
+        //Отпечатать все названия в консоль с новой строки
+        upperCaseList.forEach(System.out::println);
+    }
+
 }
